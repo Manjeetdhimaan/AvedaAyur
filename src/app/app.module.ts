87b58c5cf01d7c1214cdb220b04b55b0dd767e7d
Manjeetdhimaan <you@example.com>
2021-11-22 16:17:03 +0530

    initial commit
    Signed-off-by: Manjeetdhimaan <you@example.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './core/header/header.component';
+import { FooterComponent } from './core/footer/footer.component';
+import { HomeComponent } from './core/home/home.component';
+import { MobileMenuComponent } from './core/mobile-menu/mobile-menu.component';
+import { SignupModelComponent } from './core/signup-model/signup-model.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    MobileMenuComponent,
+    SignupModelComponent
   ],
   imports: [
     BrowserModule,
